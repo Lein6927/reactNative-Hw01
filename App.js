66977283da01757e9d0a3501dcf7d3b822704b4d
Lein6927 <lightnine861108@gmail.com>
2023-04-10 17:36:57 +0800

    add 手機號碼確認

--- App.js
+++ App.js
@@ -1,47 +1,82 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View ,TextInput} from 'react-native';
-import React,{useState} from 'react';
-import { Button } from 'react-native-web';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+//import { Button } from 'react-native-web';
 
 export default function App() {
-  
-  const [flower,chFlower]=useState('rose');
-  const [count,setCount]=useState(0);
+  const [phone, setphone] = useState('');
 
-  const changeFlower=()=>{
-    chFlower('lily')
-    console.log('變為另外一種花') 
+  const btnConfirm = () => {
+    alert(`您輸入的電話是:${phone}`)
+    console.log('確認')
+  }
+
+
+  const btnCancle = () => {
+    setphone('')
+    console.log('取消')
   }
 
   return (
     <View style={styles.container}>
-      <Text>名字:{flower}</Text>
-      <Text style={styles.wrongText}>Oops~出現錯誤了</Text>
-      <Text style={styles.mainText}>我是內文</Text>
-      <Button title='Change flower' onPress={changeFlower}></Button>
-      <Text>計數器:{count}</Text>
-      <Button title='加1'onPress={()=>setCount(count+1)}></Button>
-    {/*備註寫法*/ }
-      
+      <Text>請輸入您的電話號碼?</Text>
+
+      <TextInput
+        maxLength={10}
+        style={styles.inputText}
+        placeholder="請輸入電話..."
+        onChangeText={() => setphone()}>
+      </TextInput>
+
+       <View style={styles.buttonsContainer}>
+        <TouchableOpacity style={styles.button} onPress={btnConfirm}>
+          <Text style={styles.buttonText} >確認</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={btnCancle}>
+          <Text style={styles.buttonText} >取消</Text>
+        </TouchableOpacity>
+      </View>
+
+        {/*備註寫法*/ }
+
     </View>
   );
 }
+
+
 //style 外觀
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    //flexDirection: 'row',
     backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
-    fontSize:'20px',
+    fontSize: '20px',
   },
-  wrongText:{
-    textAlign:'center',
-    fontSize:'18px',
-    color:'red',
+  //輸入框
+  inputText: {
+    borderWidth: 1,
+    margin: '10px',
+    border: '1px solid #555',
+    borderRadius: '4px',
+    width: '25%', // 設定輸入框寬度為父容器的80%
+    marginBottom: 10, // 設定輸入框和按鈕之間的間距
   },
-  mainText:{
-    textAlign:'center',
-    fontSize:'20px',
-  }
+  //按鈕區塊
+  buttonsContainer: {
+    flexDirection: 'row',
+  },
+  //按鈕
+  button: {
+    padding: 10,
+    borderRadius: 5,
+    marginHorizontal: 20, // 設定按鈕間的間距
+    backgroundColor:'#10a2c7',
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize:'',
+  },
 });
