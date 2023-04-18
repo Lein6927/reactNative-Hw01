29e429d6e61ac69b536b9466b1dd13b84de29050
Lein6927 <lightnine861108@gmail.com>
2023-04-18 17:34:43 +0800

    coffee app

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, TouchableOpacity, } from 'react-native';
-//import { Button } from 'react-native-web';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, Image } from 'react-native';
+
+//import{ImageComponent,ImageBackground}from 'react-native';
+//import { Image } from 'react-native-web';
 
 export default function App() {
   const [inputValue, setInputValue] = useState(''); //輸入框
   const [error, setError] = useState('');
+  //const[picLogo,setpicLogo]=useState('');
+
+  //const imgCup=useState(require('./assets/coffee-04.jpg'));
 
   const handleSubmit = () => {
-    const phone=/^09\d{8}$/;//^09為開頭+後面要8碼
+    const phone = /^09\d{8}$/;//^09為開頭+後面要8碼
     if (!phone.test(inputValue)) {
       setError('請輸入正確的手機號碼');
       alert('請輸入正確的手機號碼');
@@ -19,25 +24,29 @@ export default function App() {
       console.log('成功')
     }
   };
-
+  //require('./assets/images/coffee.png')
 
   return (
     <View style={styles.container}>
-      <Text>請輸入您的手機號碼?</Text>
+      <Image style={styles.image} source={{ uri: 'https://images.plurk.com/3v2pCeK4ABEs7wg3dPjZwI.png' }}></Image>
+
+      <Text style={styles.h2Text}>來一杯咖啡吧</Text>
+      <Text style={styles.pText}>歡迎光臨，如要預約，請填寫您的手機號碼，</Text>
+      <Text style={styles.pText}>方便幫您安排座位，謝謝您</Text>
       <TextInput
         style={styles.inputText}
         value={inputValue}
         onChangeText={(text) => setInputValue(text)}
       //keyboardType="numeric"
       />
-      
+
       {error ? <Text style={styles.error}>{error}</Text> : null}
-      
+
       <TouchableOpacity
         style={styles.button}
-        title="送出"
+        title="確認"
         onPress={handleSubmit}>
-        <Text style={styles.buttonText}>送出</Text>
+        <Text style={styles.buttonText}>確認</Text>
 
       </TouchableOpacity>
 
@@ -51,6 +60,7 @@ export default function App() {
 //style 外觀
 const styles = StyleSheet.create({
   container: {
+    width: 300,
     flex: 1,
     //flexDirection: 'row',
     backgroundColor: '#FFF',
@@ -59,30 +69,49 @@ const styles = StyleSheet.create({
     fontSize: '20px',
     //borderColor:'#555',
   },
-  //輸入框
-  inputText: {
+  h2Text: { //標題字
+    color: '#A6695F',
+    fontSize: 30,
+    fontFamily: '',
+    marginBottom:5,
+  },
+  pText:{ //內文文章
+    color:'#7F4F21',
+    fontSize:18,
+  },
+
+  image: {
+    width: 240,
+    height: 180,
+    resizeMode: 'stretch',
+    marginLeft: 20,
+  },
+  inputText: {   //輸入框
     borderWidth: 1,
     margin: 10,
-    height: 20,
+    marginTop:20,
+    height: 25,
+    //width:10,
     border: '1px solid #555',
     borderRadius: '4px',
-    width: '25%', // 設定輸入框寬度為父容器的80%
+    width: '20%', // 設定輸入框寬度為父容器的80%
     marginBottom: 10, // 設定輸入框和按鈕之間的間距
   },
   button: {
     textAlign: 'center',
     padding: 10,
-    height: '30px',
-    borderRadius: '5',
+    marginTop:10,
+    height: 40,
+    width:100,
+    borderRadius: 8,
     marginHorizontal: 20, // 設定按鈕間的間距
-    borderRadius: '4px',
-    backgroundColor: '#10a2c7',
-
+    //borderRadius: '4px',
+    backgroundColor: '#A6695F',
   },
   buttonText: {
     color: '#FFF',
     fontSize: 20,
-    lineHeight: 10,//設定文字格線
+    lineHeight: 18,//設定文字格線
   },
   error: {
     color: 'red',
